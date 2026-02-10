704154d4b49e522b9aeb572a71b9f0c11ba66d02
Madhav Reddy <71065430+Madhavreddy672@users.noreply.github.com>
2026-02-10 17:15:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,27 @@
+# echo "[General]
+# path_to_gclone = ./gclone
+# telegram_token = $telegram_token
+# user_ids = $user_ids
+# group_ids = $group_ids
+# gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
+# npm install http-server -g
+# http-server -p $PORT &
+# python3 telegram_gcloner/telegram_gcloner.py
+
+
+#!/bin/bash
+set -e
+
 echo "[General]
 path_to_gclone = ./gclone
-telegram_token = $telegram_token
-user_ids = $user_ids
-group_ids = $group_ids
-gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
-npm install http-server -g
-http-server -p $PORT &
-python3 telegram_gcloner/telegram_gcloner.py
+telegram_token = ${telegram_token}
+user_ids = ${user_ids}
+group_ids = ${group_ids}
+gclone_para_override = ${gclone_para_override:-}
+" > telegram_gcloner/config.ini
+
+echo "Config written:"
+cat telegram_gcloner/config.ini
+
+# Run the bot (foreground process)
+exec python3 telegram_gcloner/telegram_gcloner.py
